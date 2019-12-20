77504753ac604442714b799efbe9f4506875727e
Vinoddangal <dangalbinod555@gmail.com>
2019-12-20 20:05:24 +0545

    My Quiz

---
+++ question.js
@@ -0,0 +1,71 @@
+var questions = [{
+	"question": "What is the capital city of Nepal ? ?",
+	"option1": "Lalitpur",
+	"option2": "Bhaktapur",
+	"option3": "Kathmandu",
+	"option4": "Pokhara",
+	"answer": "3"
+}, {
+	"question": "The first mechsnicsl computer design by charles babbage was called ?",
+	"option1": "Abacus",
+	"option2": "Analytical Engine",
+	"option3": "Calculator",
+	"option4": "Processor",
+	"answer": "2"
+}, {
+	"question": "Whic one is the first search engine in internet.",
+	"option1": "Google",
+	"option2": "Archie",
+	"option3": "Altavista",
+	"option4": "Bing",
+	"answer": "2"
+}, {
+	"question": "Whic of the following is not a Database management software.",
+	"option1": "MySQL",
+	"option2": "Oracle",
+	"option3": "Sybase",
+	"option4": "COBOL",
+	"answer": "4"
+}, {
+	"question": ".gif is an extension of.",
+	"option1": "Image File",
+	"option2": "Video File",
+	"option3": "Audio File",
+	"option4": "Word File",
+	"answer": "1"
+}, {
+	"question": "Number of bit used by the IPv6 address.",
+	"option1": "32 bit",
+	"option2": "64 bit",
+	"option3": "128 bit",
+	"option4": "256 bit",
+	"answer": "3"
+}, {
+	"question": "Firewall in computer is used for.",
+	"option1": "Security",
+	"option2": "Data Transmission",
+	"option3": "Authentication",
+	"option4": "Monitoring",
+	"answer": "1"
+}, {
+	"question": "The Number of layer in OSI model.",
+	"option1": "4",
+	"option2": "8",
+	"option3": "7",
+	"option4": "6",
+	"answer": "3"
+}, {
+	"question": "Where is the headquter of Microsoft office located.",
+	"option1": "Texas",
+	"option2": "New York",
+	"option3": "California",
+	"option4": "Washington",
+	"answer": "4"
+}, {
+	"question": "Whic protocol is used to send E-mail.",
+	"option1": "HTTP",
+	"option2": "POP3",
+	"option3": "SMTP",
+	"option4": "SSH",
+	"answer": "3"
+}];
